6ccc49aa31cb276a7b701cdaff1beb37d4f00e77
thorntonkeith <55384595+thorntonkeith@users.noreply.github.com>
2022-12-31 22:36:56 +0000

    added home component and routing

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="Test">
+        <h1> This is a test</h1>
+      </div>
+    );
+  }
+}
